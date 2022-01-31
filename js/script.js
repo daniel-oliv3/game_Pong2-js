8f0e8809167d2be7ed9bb9c890104f821748118d
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-01-31 20:25:02 -0300

    Update

---
+++ js/script.js
@@ -0,0 +1,37 @@
+var canvas, context, barraWidth, barraHeigth, jogadorPosX, jogadorPosY, 
+teclaCimaPressionada, teclaBaixoPressionada, oponentePosX,  oponentePosY
+oponenteParaCima, bolaRaio, bolaPosX, bolaPosY, bolaParaDireita, bolaAngulo
+bolaTempo, velocidadeJogador, velocidadeOponente, velocidadeBola, pontosJogador, pontoOponente;
+
+
+function iniciarJogo(){
+
+    canvas = document.getElementById("canvas");
+    context = canvas.getContext("2d");
+
+
+    barraWidth = 30;
+    barraHeigth = 90;
+    jogadorPosY = (canvas.height - barraHeigth) / 2;
+    jogadorPosX = 0;
+    teclaBaixoPressionada = false;
+    teclaCimaPressionada = false;
+
+    oponentePosX = canvas.width - barraWidth;
+    oponentePosY = 0;
+    oponenteParaCima = false;
+
+    bolaRaio = 10;
+    bolaPosX = canvas.width / 2;
+    bolaPosY = canvas.height / 2;
+
+    bolaParaDireita = false;
+    bolaAngulo = Math.floor(Math.random() * 21) - 10; //faz bola ir para uma direçao aleatoria
+    bolaTempo = 0;
+    velocidadeJogador = 15;
+    velocidadeOponente = 30;
+    velocidadeBola = 10;
+    pontosJogador = 0;
+    pontoOponente = 0;
+
+}
